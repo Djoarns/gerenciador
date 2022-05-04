dcf38985a2f48d69f5c9b2d6765d200a04f710d6
Djonatan Arns <djonatan.arns@projuris.com.br>
2022-05-04 18:39:43 -0300

    Subindo o projeto para o github

---
+++ src/main/java/br/com/alura/gerenciador/servlet/ListaEmpresasServlet.java
@@ -0,0 +1,23 @@
+package br.com.alura.gerenciador.servlet;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+@WebServlet(name = "listaEmpresas", value = "/listaEmpresas")
+public class ListaEmpresasServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        Banco banco = new Banco();
+        List<Empresa> lista = banco.getEmpresas();
+        request.setAttribute("empresas", lista);
+        RequestDispatcher rd = request.getRequestDispatcher("/listaEmpresas.jsp");
+        rd.forward(request, response);
+    }
+
+}
---
+++ src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -0,0 +1,38 @@
+package br.com.alura.gerenciador.servlet;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@WebServlet(name = "NovaEmpresa", value = "/NovaEmpresa")
+public class NovaEmpresaServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+
+        String nomeEmpresa = request.getParameter("nome");
+        Date dataAbertura;
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+            dataAbertura = sdf.parse(request.getParameter("data"));
+        } catch (ParseException e) {
+            throw new ServletException(e);
+        }
+
+        Empresa empresa = new Empresa();
+        empresa.setNome(nomeEmpresa);
+        empresa.setDataAbertura(dataAbertura);
+
+        Banco banco = new Banco();
+        banco.adiciona(empresa);
+        RequestDispatcher rd = request.getRequestDispatcher("/novaEmpresaCriada.jsp");
+        request.setAttribute("empresa", empresa.getNome());
+        rd.forward(request, response);
+    }
+}
